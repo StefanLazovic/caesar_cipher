213f6f3ac7db88c871ced46b07f711e42446b480
Stefan Lazovic <agios.stefanos@hotmail.com>
2020-02-27 17:13:32 +0100

    pivot alphabet array, cipherArray is getting values based on a shifted alphabetical array

--- src/components/TextToCipher.jsx
+++ src/components/TextToCipher.jsx
@@ -5,8 +5,27 @@ class TextToCipher extends Component {
     input: {
       //////////  0    1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25
       alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-      shifalph: ['d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c']
+      shifalph: ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c"],
+      shiftedAlphabet: []
     }
+  }
+
+  // warning! some lifecycle methods are going to be renamed very soon
+  componentWillMount() {
+    this.pivotArray();
+  }
+
+
+  pivotArray = () => {
+    const { alphabet } = this.state.input;
+    const slice1 = alphabet.slice(0, 3);
+    const slice2 = alphabet.slice(3, 26);
+    this.setState({
+      input: {
+        ...this.state.input,
+        shiftedAlphabet: [...slice2, ...slice1]
+      }
+    });
   }
 
 
@@ -16,21 +35,27 @@ class TextToCipher extends Component {
         ...this.state.input,
         [e.target.name]: e.target.value
       }
-    })
+    });
   }
 
+
   tryCipher = () => {
-    const { alphabet, text } = this.state.input;
+    const { alphabet, shiftedAlphabet, text } = this.state.input;
     if (text) {
-      let textArray = [];
+      const textArray = [];
+      const textIndexArray = [];
+      const cipherArray = [];
       for (let i = 0; i < text.length; i++) {
         textArray.push(text[i]);
         if (alphabet.includes(textArray[i])) {
-          console.log(alphabet.indexOf(textArray[i]));
+          textIndexArray.push(alphabet.indexOf(textArray[i]));
+          cipherArray.push(shiftedAlphabet[textIndexArray[i]]);
+          console.log(cipherArray);
         }
       }
     }
   }
+
 
   render() {console.log(this.state);
     const { text, cipheredText } = this.state.input;
@@ -48,6 +73,7 @@ class TextToCipher extends Component {
       </div>
     );
   }
+
 
 }
 
