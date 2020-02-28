4eed2b05e0f8038a048da5a841b8ed7f8ede39a9
Stefan Lazovic <agios.stefanos@hotmail.com>
2020-02-28 16:57:02 +0100

    cipher decoded, updated changes about setState method

--- src/components/TextToCipher.jsx
+++ src/components/TextToCipher.jsx
@@ -6,12 +6,14 @@ class TextToCipher extends Component {
       //////////  0    1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25
       alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
       shifalph: ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c"],
-      shiftedAlphabet: []
+      shiftedAlphabet: [],
+      encodedText: '',
+      decodedText: ''
     }
   }
 
-  // warning! some lifecycle methods are going to be renamed very soon
-  componentWillMount() {
+
+  componentDidMount() {
     this.pivotArray();
   }
 
@@ -29,47 +31,67 @@ class TextToCipher extends Component {
   }
 
 
-  handleChange = (e) => {
+
+  tryCipher = (e) => {
+    const { alphabet, shiftedAlphabet, text, encodedText } = this.state.input;
+    const eventText = e.target.value;
+    const textArray = [];
+    const textIndexArray = [];
+    const cipherArray = [];
+    let cipher = '';
+    for (let i = 0; i < eventText.length; i++) {
+      textArray.push(eventText[i]);
+      if (alphabet.includes(textArray[i])) {
+        textIndexArray.push(alphabet.indexOf(textArray[i]));
+        cipherArray.push(shiftedAlphabet[textIndexArray[i]]);
+        cipher = cipherArray.join('');
+      }
+    }
     this.setState({
       input: {
         ...this.state.input,
-        [e.target.name]: e.target.value
+        encodedText: cipher
       }
     });
   }
 
 
-  tryCipher = () => {
-    const { alphabet, shiftedAlphabet, text } = this.state.input;
-    if (text) {
-      const textArray = [];
-      const textIndexArray = [];
-      const cipherArray = [];
-      for (let i = 0; i < text.length; i++) {
-        textArray.push(text[i]);
-        if (alphabet.includes(textArray[i])) {
-          textIndexArray.push(alphabet.indexOf(textArray[i]));
-          cipherArray.push(shiftedAlphabet[textIndexArray[i]]);
-          console.log(cipherArray);
-        }
+  decodeCipher = (e) => {
+    const { alphabet, shiftedAlphabet, encodedText } = this.state.input;
+    const eventText = e.target.value;
+    const cipherArray = [];
+    const cipherIndexArray = [];
+    const textArray = [];
+    let text = '';
+    for (let i = 0; i < eventText.length; i++) {
+      cipherArray.push(eventText[i]);
+      if (shiftedAlphabet.includes(cipherArray[i])) {
+        cipherIndexArray.push(shiftedAlphabet.indexOf(cipherArray[i]));
+        textArray.push(alphabet[cipherIndexArray[i]]);
+        text = textArray.join('');
       }
     }
+    this.setState({
+      input: {
+        ...this.state.input,
+        decodedText: text
+      }
+    });
   }
 
 
-  render() {console.log(this.state);
-    const { text, cipheredText } = this.state.input;
+  render() {console.log(this.state.input);
+    const { encodedText, decodedText } = this.state.input;
     return (
       <div>
         <form>
           <label htmlFor="text">Plain Text</label><br/>
-          <input type="text" id="text" name="text" onChange={this.handleChange} /><br/>
+          <input type="text" id="text" name="text" onChange={this.tryCipher} /><br/>
 
-          <label htmlFor="cipheredText">Ciphered Text</label><br/>
-          <input type="text" id="cipheredText" name="cipheredText" onChange={this.handleChange} />
+          <label htmlFor="encodedText">Ciphered Text</label><br/>
+          <input type="text" id="encodedText" name="encodedText" onChange={this.decodeCipher} />
+          <p>{decodedText}</p>
         </form>
-        <p>{this.tryCipher()}</p>
-        <p>{cipheredText}</p>
       </div>
     );
   }
