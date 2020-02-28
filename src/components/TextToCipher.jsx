f16d59086582b46c3645702058a4d5bbe0fefe6a
Stefan Lazovic <agios.stefanos@hotmail.com>
2020-02-28 18:16:14 +0100

    code refactoring

--- src/components/TextToCipher.jsx
+++ src/components/TextToCipher.jsx
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 
 class TextToCipher extends Component {
   state = {
-    input: {
-      //////////  0    1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25
-      alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-      shifalph: ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c"],
-      shiftedAlphabet: [],
-      encodedText: '',
-      decodedText: ''
-    }
+    //////////  0    1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25
+    alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    shiftedAlphabet: [],
+    encodedText: '',
+    decodedText: ''
   }
+
 
 
   componentDidMount() {
@@ -18,70 +16,48 @@ class TextToCipher extends Component {
   }
 
 
+
   pivotArray = () => {
-    const { alphabet } = this.state.input;
+    const { alphabet } = this.state;
     const slice1 = alphabet.slice(0, 3);
     const slice2 = alphabet.slice(3, 26);
     this.setState({
-      input: {
-        ...this.state.input,
-        shiftedAlphabet: [...slice2, ...slice1]
-      }
+      ...this.state,
+      shiftedAlphabet: [...slice2, ...slice1]
     });
   }
 
 
 
   tryCipher = (e) => {
-    const { alphabet, shiftedAlphabet, text, encodedText } = this.state.input;
-    const eventText = e.target.value;
-    const textArray = [];
-    const textIndexArray = [];
-    const cipherArray = [];
+    const { alphabet, shiftedAlphabet } = this.state;
     let cipher = '';
-    for (let i = 0; i < eventText.length; i++) {
-      textArray.push(eventText[i]);
-      if (alphabet.includes(textArray[i])) {
-        textIndexArray.push(alphabet.indexOf(textArray[i]));
-        cipherArray.push(shiftedAlphabet[textIndexArray[i]]);
-        cipher = cipherArray.join('');
-      }
+    for (const letter of e.target.value) {
+      cipher += shiftedAlphabet[alphabet.indexOf(letter)];
     }
     this.setState({
-      input: {
-        ...this.state.input,
-        encodedText: cipher
-      }
+      ...this.state,
+      encodedText: cipher
     });
   }
+
 
 
   decodeCipher = (e) => {
-    const { alphabet, shiftedAlphabet, encodedText } = this.state.input;
-    const eventText = e.target.value;
-    const cipherArray = [];
-    const cipherIndexArray = [];
-    const textArray = [];
+    const { alphabet, shiftedAlphabet } = this.state;
     let text = '';
-    for (let i = 0; i < eventText.length; i++) {
-      cipherArray.push(eventText[i]);
-      if (shiftedAlphabet.includes(cipherArray[i])) {
-        cipherIndexArray.push(shiftedAlphabet.indexOf(cipherArray[i]));
-        textArray.push(alphabet[cipherIndexArray[i]]);
-        text = textArray.join('');
-      }
+    for (const letter of e.target.value) {
+      text += alphabet[shiftedAlphabet.indexOf(letter)];
     }
     this.setState({
-      input: {
-        ...this.state.input,
-        decodedText: text
-      }
+      ...this.state,
+      decodedText: text
     });
   }
 
 
-  render() {console.log(this.state.input);
-    const { encodedText, decodedText } = this.state.input;
+
+  render() {console.log(this.state);
     return (
       <div>
         <form>
@@ -90,13 +66,10 @@ class TextToCipher extends Component {
 
           <label htmlFor="encodedText">Ciphered Text</label><br/>
           <input type="text" id="encodedText" name="encodedText" onChange={this.decodeCipher} />
-          <p>{decodedText}</p>
         </form>
       </div>
     );
   }
-
-
 }
 
 export default TextToCipher;
